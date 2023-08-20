ece66e63ffb1529db048b1aa22f0ba6b16f7c3b7
Alejandro-12 <h.alejandro1021@gmail.com>
2023-08-20 17:48:51 -0600

    servidor-1

---
+++ servidorLista.js
@@ -0,0 +1,45 @@
+const http = require('http');
+
+const port = 3000;
+
+//lista
+const listaDeTareas = [
+  {
+    "id": 1,
+    "tarea": "Hacer la tarea de matemáticas",
+    "completada": false
+  },
+  {
+    "id": 2,
+    "tarea": "Lavar los platos",
+    "completada": true
+  },
+  {
+    "id": 3,
+    "tarea": "Estudiar para el examen",
+    "completada": false
+  }
+];
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/lista') {
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(200);
+    res.end(JSON.stringify({ tareas: listaDeTareas }));
+
+  } else {
+    res.writeHead(404);
+    res.end('Ruta no encontrada');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Servidor Corriendo ${port}`);
+});
+
+
+
+
+server.listen(port,()=>{
+    console.log('Servidor corriendo')
+})
